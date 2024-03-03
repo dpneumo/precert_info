b0b3bbda0e2f1500f1a070a3c510669de32b9a90
dpneumo <dpneumo@gmail.com>
2024-03-02 18:04:33 -0600

    new p2p and contacts are entered via modal dialog. Added some tailwindcss based classes. Some clean up

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.dataset.action = "modal#show";
+  }
+ 
+   show(event) {
+    const dialog = document.getElementById(event.params.dialog);
+    dialog.showModal();
+  }
+} 
