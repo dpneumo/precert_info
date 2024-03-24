29ebae7b24209939c1d14756ca814c94f8ac8ca7
dpneumo <dpneumo@gmail.com>
2024-03-23 23:17:28 -0500

    Order insurers, offices, & providers by name in index views

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -3,7 +3,7 @@ class OfficesController < ApplicationController
 
   # GET /offices or /offices.json
   def index
-    @pagy, @offices = pagy(Office.all, items: 12)
+    @pagy, @offices = pagy(Office.order(:name), items: 12)
   end
 
   # GET /offices/new
